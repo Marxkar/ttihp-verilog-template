7861cdff338a78c6eb01edd8ce4979ec0741fc75
Marxkar <129550084+Marxkar@users.noreply.github.com>
2025-06-19 13:19:58 +0530

    Update dff.v

--- src/dff.v
+++ src/dff.v
@@ -1,9 +1,9 @@
-module dff(input clock,reset,d,output q,qb);
+module dff(input clk,reset,d,output q,qb);
 wire [4:0]a;
 assign a[0]=~reset;
 assign a[1]=~(a[0]&d&a[2]);
-assign a[2]=~(a[1]&clock&a[3]);
-assign a[3]=~(a[0]&clock&a[4]);
+assign a[2]=~(a[1]&clk&a[3]);
+assign a[3]=~(a[0]&clk&a[4]);
 assign a[4]=~(a[1]&a[3]);
 assign qb=~(a[0]&a[2]&q);
 assign q=~(a[3]&qb);

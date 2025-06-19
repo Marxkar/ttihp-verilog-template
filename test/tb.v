8cce535583254737349445b444d2c438c409a10d
Marxkar <129550084+Marxkar@users.noreply.github.com>
2025-06-19 18:59:00 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,14 +1,14 @@
 module tbseq;
-reg reset;
+reg rst_n;
 reg clk=0;
 reg input_bit;
 wire output_indicator;
 wire [2:0]present_state;
-  seqdetc uut(clk,reset,input_bit,ena,output_indicator,present_state);
+  seqdetc uut(clk,rst_n,input_bit,ena,output_indicator,present_state);
 always #5 clk=~clk;
 initial begin
-reset=1;#7;
-reset=0;input_bit=1;#3;
+rst_n=0;#7;
+rst_n=1;input_bit=1;#3;
 input_bit=0;#10;
 input_bit=1;#10;
 input_bit=0;#10;

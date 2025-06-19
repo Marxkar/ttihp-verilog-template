8197045ecf9cccbfe8b2874d2c5db1663b24959f
Marxkar <129550084+Marxkar@users.noreply.github.com>
2025-06-19 13:19:27 +0530

    Update tt_um_marxkar_seqdetc.v

--- src/tt_um_marxkar_seqdetc.v
+++ src/tt_um_marxkar_seqdetc.v
@@ -1,4 +1,4 @@
-module tt_um_marxkar_seqdetc(input clock,reset,input_bit,output reg output_indicator,output wire [2:0]present_state);
+module tt_um_marxkar_seqdetc(input clk,reset,input_bit,output reg output_indicator,output wire [2:0]present_state);
 wire [2:0]qb;
 wire[2:0]d;
 assign d[2]=(~present_state[2]&present_state[1]&present_state[0]&~input_bit);
@@ -6,9 +6,9 @@ assign d[1]=(~present_state[2]&~present_state[1]&present_state[0]&~input_bit)
             |(~present_state[2]&present_state[1]&~present_state[0]&input_bit);
 assign d[0]=input_bit&(~present_state[2]|(present_state[2]&~present_state[1]&~present_state[0])) 
             |present_state[2]&~present_state[1]&present_state[0]&~input_bit;
-dff d0(clock,reset,d[2],present_state[2],qb[2]);
-dff d1(clock,reset,d[1],present_state[1],qb[1]);
-dff d2(clock,reset,d[0],present_state[0],qb[0]);
-always @(posedge clock)
+dff d0(clk,reset,d[2],present_state[2],qb[2]);
+dff d1(clk,reset,d[1],present_state[1],qb[1]);
+dff d2(clk,reset,d[0],present_state[0],qb[0]);
+always @(posedge clk)
 output_indicator<=present_state[2]&~present_state[1]&~present_state[0]&input_bit;
 endmodule
